cae132e0938290973940e01b69e8a0710273e233
patelboss <83952959+patelboss@users.noreply.github.com>
2024-12-19 19:06:41 +0530

    Update forward.py

--- plugins/forward.py
+++ plugins/forward.py
@@ -134,7 +134,7 @@ async def forward(bot, message):
                     except:
                         pass
                 except FloodWait as e:
-                    await asyncio.sleep(e.x)
+                    await asyncio.sleep(e.value)
                     if file_type in ("document", "video", "audio", "photo"):
                         await bot.send_cached_media(
                             chat_id=chat_id,
