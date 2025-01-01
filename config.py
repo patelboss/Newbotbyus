e4b1978d154c6e6cef6f99a06f1bc2155203edce
patelboss <83952959+patelboss@users.noreply.github.com>
2025-01-01 16:45:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,8 @@ DATABASE_URI = os.environ.get("DATABASE_URI", "")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 COLLECTION_NAME = os.environ.get("COLLECTION_NAME", "Forward_data")
 SESSION = os.environ.get("SESSION", "BQE0_ZkAjmev5JZRKYZQfyJPD_pZdpzpa22I9SS2RmYXl6kKQJjnFYJjm2IJVWABMdo-zNPWIS33gk82FbpZMKjD97KeFBz1JNM8CQTfQ7sm_GuR5hgfir5ccRuKyyLUXKumpfFm35KhJzpThBORq-jKyr5SXX1qwCSerzKX7yAln1AkVoK77VP8HyIFy3dRK5RZWa1XpHPuRWLX8neiCoun3Xh9BlA8hmD-29gATa3q4ryKL3MeD0Vg9g6FgB3ao3c5zIGb7o0zkhFbdnyg3t_w8q-xgucT8YbWoLmVO0pdeDmT-Bi3kPogmUCSpDF0iAhuc8IeqTe4PohPgooiUGR_268HIQAAAAB-RdJ7AA")
-TO_CHANNEL = int(os.environ.get("TO_CHANNEL", 0))  # Default to 0 if not set
+TO_CHANNEL = int(os.environ.get("TO_CHANNEL", 0)) 
+print(f"To Channel : {TO_CHANNEL}") #Default to 0 if not set
 
 # Logger Function
 def LOGGER(name: str) -> logging.Logger:
